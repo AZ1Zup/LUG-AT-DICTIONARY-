31970bf68bebea2d1bd787c93743d82404a353e0
AZ1Zup <125564472+AZ1Zup@users.noreply.github.com>
2023-02-21 21:45:45 +0500

    New lessons
    Yangi darslar (LUG'AT)

---
+++ LUG'AT (DICTIONARY).py
@@ -0,0 +1,14 @@
+cars_1 = {'ferrari':'bor', 'lamorgini':'bor', 'gentra':"yo'q"}
+print(cars_1['ferrari'])
+
+
+mevalar = {'olma':'10000', 'banan':'7000', 'uzum':'6000'}
+print(f"Bu mevani narxi {mevalar['olma']} so'm ")
+
+malumot = {'ism':'aziz', 'yosh':'13','yil':'2010'}
+print(f"{malumot['ism'].title()} {malumot['yosh']}-yoshda {malumot['yil']}-yilda")
+
+# del - fuunksiyasi bu funksiya biror bir ro'yxat ichidagi
+# qiymatni o'chirib beradi bundan foydalanish uchun:
+# del mevalar['banan'] -- qilsak bizda "mevalar"
+# ro'yxatidan banan o'chirilib yuboriladi.
